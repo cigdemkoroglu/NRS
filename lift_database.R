151e7b697de8d2ae6967ed9be6bf45bd4d91b414
Serdar Altok <95932745+serdar-altok@users.noreply.github.com>
2024-05-05 21:22:10 -0700

    Update lift_database.R

--- lift_database.R
+++ lift_database.R
@@ -3,13 +3,15 @@ lift_database <- function(database, insertions_df){
   library(data.table)
   library(magrittr)
   
+  # read the input files
   database_dt <- fread(database)
   names(database_dt)[1]  <- 'CHROM'
-  
   insertions_df <- as.data.frame(readxl::read_xlsx(insertions_df))
-  
+
+  # add deletion sizes
   insertions_df$deleted_length <- insertions_df$END - insertions_df$START + 1
 
+  # remove the rows which intersect with an insertion
   database_dt[, remove_row := FALSE]
   for (i in 1:nrow(insertions_df)){
     chromosome <- paste0('chr', insertions_df$CHROM[i])
@@ -17,10 +19,12 @@ lift_database <- function(database, insertions_df){
   }
   
   database_dt <- database_dt[remove_row == FALSE,]
-  
   database_dt[,remove_row := NULL]
-  
+
+  # record the original positions
   database_dt[, old_POS := POS]
+
+  # reflect the changes after insertions and deletions to the positions
   for (i in 1:nrow(insertions_df)){
     chromosome <- paste0('chr', insertions_df$CHROM[i])
     database_dt[CHROM == chromosome & old_POS >= insertions_df$START[i],
@@ -28,7 +32,8 @@ lift_database <- function(database, insertions_df){
   }
   
   database_dt[ ,old_POS := NULL]
-  
+
+  # write the results
   names(database_dt)[1] <- '#CHROM'
   fwrite(database_dt, './database_modified.vcf.txt', sep = '\t')
   return(database_dt)
