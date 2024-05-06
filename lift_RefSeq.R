a15a9b8c1b79ed9b82209692bab8be061913ad24
Serdar Altok <95932745+serdar-altok@users.noreply.github.com>
2024-05-05 18:43:05 -0700

    Update lift_RefSeq.R

--- lift_RefSeq.R
+++ lift_RefSeq.R
@@ -1,43 +1,52 @@
-lift_RefSeq <- function(database_file, insertions_file, names_file){
+lift_RefSeq <- function(database_file, insertions_file){
   
   library(data.table)
   
   # sample call
-  # lift_RefSeq('./lift_RefSeq/hg38_refGene.txt', './lift_RefSeq/insertions_v5.xlsx', './lift_RefSeq/RefSeq_changes.xlsx')
-  
+  # lift_RefSeq('./lift_RefSeq/hg38_refGene.txt', './lift_RefSeq/insertions_v5.xlsx')
+
+  # read input files
   database_dt <- fread(database_file)
-  df_for_names <- as.data.frame(readxl::read_xlsx(names_file))
   insertions_df <- as.data.frame(readxl::read_xlsx(insertions_file))
-  
+
+  # adjust column names
   original_names <- names(database_dt) # save original column names to convert back at the end
-  names(database_dt)  <- names(df_for_names) # use actual column names from the other file
-  database_dt[,move_amount := 0] # move_amount column =
-  database_dt[,change_flag := character()]
+  names(database_dt)  <- c('bin', 'name', 'chrom', 'strand', 'txStart', 'txEnd', 'cdsStart', 'cdsEnd', 'exonCount', 'exonStarts', 'exonEnds', 'score', 'name2', 'cdsStartStat', 'cdsEndStat', 'exonFrame')
   
+  database_dt[,move_amount := 0] # initialize column for move amount
+  database_dt[,change_flag := character()] # flag to keep track of changed rows
+
+  # add deletion length
   insertions_df$deleted_length <- insertions_df$END - insertions_df$START + 1
-  
+
+  # filter out rows with invaid chromosome name
   valid_chrs <- c(paste0('chr', 1:23), 'chrX', 'chrY')
   database_dt <- database_dt[chrom %in% valid_chrs,]
   
   columns_to_modify <- c('txStart', 'txEnd', 'cdsStart', 'cdsEnd', 'exonStarts', 'exonEnds')
-  
+
+  # make a copy of the columns that will be modified
   database_dt[, (paste0('old_', columns_to_modify)) := .SD, .SDcols = columns_to_modify]
   modified_gene_names <- c()
   
   for (i in 1:nrow(insertions_df)){
-    
+
+    # read info from ith row of insertions file
     chromosome <- paste0('chr', insertions_df$CHROM[i])
     start <- insertions_df$START
     end <- insertions_df$END
     insertion_size = insertions_df$insert_length[i] - insertions_df$deleted_length[i]
-    
+
+    # for each row in the database, add insertion size to movement amount if the gene sequence in database comes after the insertion
     database_dt[chrom == chromosome & txStart >= insertions_df$START[i],
                 move_amount := move_amount + insertion_size]
-    
+
+    # using the flag column, note the row id of the insertion, if the insertion falls in the gene sequence 
     database_dt[chrom == chromosome & txStart <= insertions_df$START[i] & txEnd >= insertions_df$START[i], change_flag := i]
     
   }
-  
+
+  # function for moving exons
   move_exons <- function(exons, total_move_amount, insertion_size=0, start=0){
     exons <- as.numeric(unlist(strsplit(exons, ',')))
     for (i in 1:length(exons)){
@@ -50,25 +59,31 @@ lift_RefSeq <- function(database_file, insertions_file, names_file){
   }
   
   percent_cut = nrow(database_dt)%/%100
-  
+
+  # go over the database to add the total movements to each row
   for (i in 1:nrow(database_dt)){
     
     if (i %% percent_cut == 0){
       percent = i / percent_cut
       {cat('\r', percent, '%')}
     }
+
+    # add movements to start and end points
     database_dt[i, txStart := txStart + move_amount]
     database_dt[i, txEnd := txEnd + move_amount]
     database_dt[i, cdsStart := cdsStart + move_amount]
     database_dt[i, cdsEnd := cdsEnd + move_amount]
-    
+
+    # get exon info
     exon_starts <- database_dt$exonStarts[i]
     exon_ends <- database_dt$exonEnds[i]
     move_amount <- database_dt$move_amount[i]
-    
+
+    # move exon starts and ends for the insertions before the gene sequence
     database_dt[i, exonStarts := move_exons(exon_starts, move_amount)]
     database_dt[i, exonEnds := move_exons(exon_ends, move_amount)]
-    
+
+    # move exon starts and ends for the insertion that falls into the gene sequence
     if(!is.na(database_dt$change_flag[i])){
       flag <- as.integer(database_dt$change_flag[i])
       start <- insertions_df$START[flag]
@@ -85,9 +100,11 @@ lift_RefSeq <- function(database_file, insertions_file, names_file){
       }
     }
   }
-  
+
+  # delete original columns
   database_dt[, (paste0('old_', columns_to_modify)) := NULL]
-  
+
+  # write to the output file
   names(database_dt)[1] <- '#CHROM'
   fwrite(database_dt, './database_modified.txt', sep = '\t')
   
